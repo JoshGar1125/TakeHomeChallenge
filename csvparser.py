49609ab46d08a7ee7e02291e5f9bea2a0f3b2d31
J-Dawg132 <111777662+J-Dawg132@users.noreply.github.com>
2025-05-15 18:18:19 -0500

    Printed output, correct

---
+++ csvparser.py
@@ -0,0 +1,67 @@
+import csv
+import collections
+
+typePoints = collections.Counter()
+
+def data_points(data):
+    count = 0 
+    for line in data:
+        count += 1 #counter
+    return count
+
+def count_types(rows):
+    pointsType = []
+    for row in rows:
+        if len(row) >= 4:
+            typePoints[row[3]] += 1
+
+    for key in sorted(typePoints):
+        pointsType.append(f"{key}: {typePoints[key]}")
+    
+    return (', '.join(pointsType))
+
+def maintenance(rows):
+    needMaint = []
+    for row in rows:
+        if int(row[5]) < 3:
+            needMaint.append(row[0])
+    return(', '.join(needMaint))
+
+def inspection(rows):
+    lastInspect = []
+    for row in rows:
+        if row[6] < "2024-01-01":
+            lastInspect.append(row[0])
+    return(', '.join(lastInspect))
+
+def minmaxBounds(row):
+    Latitude, Longitude = [], []
+    for row in rows:
+        Latitude.append(float(row[1]))
+        Longitude.append(float(row[2]))
+    minLat = min(Latitude)
+    maxLat = max(Latitude)
+    minLong = min(Longitude)
+    maxLong = max(Longitude)
+    return minLat, maxLat, minLong, maxLong
+
+#read the CSV file
+with open('gis_data.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    data = list(csv_reader)
+
+    header = data[0] 
+    rows = data[1:] #all other data, no header
+
+    count = data_points(rows)
+    types = count_types(rows)
+    maint = maintenance(rows)
+    inspect = inspection(rows)
+    minValLat, maxValLat, minValLong, maxValLong  = minmaxBounds(rows)
+    
+    print("Total number of points:", count)
+    print("Count of points by type:", types)
+    print("Points requiring maintenance:", maint)
+    print("Points not inspected in the last year include:", inspect)
+    print("The Min Lat value is:", minValLat, "| Max Lat:","%.4f" % maxValLat, "| Min Long:", minValLong, "| Max Long:", "%.4f" % maxValLong)    
+    
